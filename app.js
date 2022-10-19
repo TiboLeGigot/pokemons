fb2d901e00071c3a2a4bec1a5d0d86f4978078ad
Thibault <t.jeannequin@gmail.com>
2022-10-19 15:36:48 +0200

    [07:09] Deploiement Heroku

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 const express = require('express')
-const morgan = require('morgan')
 const favicon = require('serve-favicon')
 const bodyParser = require('body-parser')
 const sequelize = require('./src/db/sequelize')
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app
     .use(favicon(__dirname + '/favicon.ico'))
-    .use(morgan('dev'))
     .use(bodyParser.json())
 
 sequelize.initDb()
+
+app.get('/', (req, res) => {
+    return res.json('Hello Heroku !')
+})
 
 // Endpoints
 require('./src/routes/findAllPokemons')(app)
@@ -23,9 +25,9 @@ require('./src/routes/deletePokemon')(app)
 require('./src/routes/login')(app)
 
 // Errors
-app.use(({res}) => {
+app.use(({ res }) => {
     const message = "Impossible de trouver la ressource demandée."
-    res.status(404).json({message});
+    res.status(404).json({ message });
 })
 
 app.listen(port, () => console.log(`Notre application Node est démarrée sur : http://localhost:${port}`))
